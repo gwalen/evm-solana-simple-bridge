64260680b8c1b4546ed604a8d2d32474a6613fc2
Grzegorz Walen <gwalen@github>
2025-03-19 10:44:29 +0100

    Add function to integration to intialize solana programs and accounts

--- solana-node/tests/consts.ts
+++ solana-node/tests/consts.ts
@@ -1,5 +1,10 @@
 import { Keypair } from "@solana/web3.js";
+import * as dotenv from 'dotenv';
 
-export const OWNER = Keypair.generate();
-export const RELAYER = Keypair.generate();
+dotenv.config();
+
 export const MINT_DECIMALS = 6;
+
+export const ALICE = Keypair.fromSecretKey(new Uint8Array(JSON.parse(process.env.ALICE_PRIVATE_KEY!) as number[]));
+export const OWNER = Keypair.fromSecretKey(new Uint8Array(JSON.parse(process.env.OWNER_PRIVATE_KEY!) as number[]));
+export const RELAYER = Keypair.fromSecretKey(new Uint8Array(JSON.parse(process.env.RELAYER_PRIVATE_KEY!) as number[]));
